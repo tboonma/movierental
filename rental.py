0e332a8fc67758e8224072df81e70d3258b53461
tboonma <tboonma@hotmail.com>
2021-10-13 19:25:00 +0700

    Improve code quality by flake8

--- rental.py
+++ rental.py
@@ -1,35 +1,30 @@
-import logging
-
-from movie import Movie, PriceCode
-
-
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	But for simplicity of the example only a days_rented
-	field is used.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
-		   a movie with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    But for simplicity of the example only a days_rented
+    field is used.
+    """
 
-	def get_movie(self):
-		return self.movie
+    def __init__(self, movie, days_rented):
+        """Initialize a new movie rental object for
+           a movie with known rental period (daysRented).
+        """
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_movie(self):
+        return self.movie
 
-	def get_price(self):
-		return self.movie.price_code.price(self.days_rented)
+    def get_days_rented(self):
+        return self.days_rented
 
-	def get_renter_points(self):
-		return self.movie.price_code.renter_points(self.days_rented)
+    def get_price(self):
+        return self.movie.price_code.price(self.days_rented)
+
+    def get_renter_points(self):
+        return self.movie.price_code.renter_points(self.days_rented)
