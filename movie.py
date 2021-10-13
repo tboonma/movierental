3c752c1bd5ab074a7acfe3c1b22bad46a4166b3e
tboonma <tboonma@hotmail.com>
2021-10-13 17:37:00 +0700

    Refactored duplicated method

--- movie.py
+++ movie.py
@@ -6,7 +6,7 @@ class Movie:
 	REGULAR = 0
 	NEW_RELEASE = 1
 	CHILDRENS = 2
-	
+
 	def __init__(self, title, price_code):
 		# Initialize a new movie. 
 		self.title = title
@@ -15,9 +15,6 @@ class Movie:
 	def get_price_code(self):
 		# get the price code
 		return self.price_code
-	
-	def get_title(self):
-		return self.title
-	
+
 	def __str__(self):
 		return self.title
